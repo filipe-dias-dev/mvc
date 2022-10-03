fc3daa2b5b46ed6e058985ef77ff56947c9da6b7
filipe <filipe.dias09@hotmail.com>
2022-10-03 20:02:30 -0300

    sistema de roteamento

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ app.get('/', (req, res)=> res.send('Olá Filipe!'));
 
 app.get('./contatos', (req, res) => res.send("Página contatos"));
 
+/**
+ * Sistema de roteamento
+ */
+
 app.use('/produtos', rotasProdutos);
 
 /**
  * Rotas Parametrizadas
  */
 
-app.get('produtos/:id', (req, res)=> {
-  let id = req.params.id
-})
+app.get('/produtos/:id', (req, res)=> {
+  let {id} = req.params;
+  console.log('Eu tenho um produto com o id: ', id)
+});
 
 /**
  * Criando um servidor
--- controllers/ProdutoController.js
+++ controllers/ProdutoController.js
@@ -9,5 +9,4 @@ const ProdutoController = {
     },
 }
 
-
 module.exports = ProdutoController;
